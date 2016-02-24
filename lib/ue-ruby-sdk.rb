0816f7b8d15cdbb7935d5b3dee015d8874eb7455
Hossam Saraya <hossam.saraya@gmail.com>
2016-02-24 02:50:47 +0200

    Logfixing

--- lib/ue-ruby-sdk.rb
+++ lib/ue-ruby-sdk.rb
@@ -7,5 +7,6 @@ $logger.warn "ENTRYPOINT"
 #TODO: Remove before release
 
 app = UEApp.new("2206779c2acc42e2824c62026001ac25","65c9ab904a3354738a4442e798f27387")
-binding.pry
+puts app.list_users
+# binding.pry
 puts "X"
--- lib/utilities/UELogger.rb
+++ lib/utilities/UELogger.rb
@@ -1,5 +1,26 @@
 require 'logging'
 
-$logger = Logging.logger(STDOUT)
+Logging.color_scheme( 'bright',
+    :levels => {
+        :info  => :green,
+        :warn  => :yellow,
+        :error => :red,
+        :fatal => [:white, :on_red]
+    },
+    :date => :blue,
+    :logger => :cyan,
+    :message => :magenta
+)
+
+Logging.appenders.stdout(
+    'stdout',
+    :layout => Logging.layouts.pattern(
+        :pattern => '[%d] %-5l %c: %m\n',
+        :color_scheme => 'bright'
+    )
+)
+
+$logger = Logging.logger['UE']
+$logger.add_appenders 'stdout'
 $logger.level = :debug
 
